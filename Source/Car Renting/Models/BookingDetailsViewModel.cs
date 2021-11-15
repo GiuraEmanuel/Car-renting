6e8affc52db82937c63f719a54b984e58a587f1a
Giura Emanuel <emanuel1996a@gmail.com>
2021-11-15 19:15:36 +0200

    Added more booking info to booking detail page

--- Source/Car Renting/Models/BookingDetailsViewModel.cs
+++ Source/Car Renting/Models/BookingDetailsViewModel.cs
@@ -5,21 +5,28 @@ namespace Car_Renting.Models
     {
         public DateTime StartDate { get; }
         public DateTime EndDate { get; }
-        public string Manufacturer { get; } 
+        public string Manufacturer { get; }
         public string Model { get; }
         public int TotalNumberOfDays => (EndDate - StartDate).Days;
         public decimal TotalCost { get; }
-
+        public string Name { get; }
+        public string Email { get; }
+        public string PhoneNumber { get; }
+        public string BookingNumber { get; }
 
 
         public BookingDetailsViewModel(DateTime startDate, DateTime endDate,
-            string manufacturer, string model, decimal totalCost)
+            string manufacturer, string model, decimal totalCost, string name, string email, string phoneNumber, string bookingNumber)
         {
             StartDate = startDate;
             EndDate = endDate;
             Manufacturer = manufacturer;
             Model = model;
             TotalCost = totalCost;
+            Name = name;
+            Email = email;
+            PhoneNumber = phoneNumber;
+            BookingNumber = bookingNumber;
         }
     }
 }
