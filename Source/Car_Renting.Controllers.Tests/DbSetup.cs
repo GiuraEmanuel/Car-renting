22ecac93ab8cc37942a4c02313373c94fad9442a
Giura Emanuel <emanuel1996a@gmail.com>
2021-11-27 19:47:16 +0200

    Added mocks for database and required test data

---
+++ Source/Car_Renting.Controllers.Tests/DbSetup.cs
@@ -0,0 +1,69 @@
+﻿using Car_Renting.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+namespace Car_Renting.Controllers.Tests
+{
+    public static class DbSetup
+    {
+        public static AppDbContext Initialize()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseSqlServer("Server=(localdb)\\CarRentingTest;Database=Car-Renting-Test;Trusted_Connection=True;MultipleActiveResultSets=true");
+            var dbContext = new AppDbContext(optionsBuilder.Options);
+            dbContext.Database.EnsureDeleted();
+            dbContext.Database.EnsureCreated();
+            return dbContext;
+        }
+
+        public static (MockUserManager UserManager, User AdminUser, User User) SeedUsers(this AppDbContext context)
+        {
+            var userManager = MockUserManager.Create(context);
+            var admin = userManager.AddUser("giura.emanuel@gmail.com", "Giura", "Emanuel", "222555666", true); // admin
+            var user = userManager.AddUser("jason.bourne@gmail.com", "Jason", "Bourne", "555777999", false); // normal user;
+
+            return (userManager, admin, user);
+        }
+        /// <summary>
+        /// Adds a list of 4 cars to the database.
+        /// </summary>
+        /// <param name="appDbContext">Database context.</param>
+        /// <returns> Returns a list of cars containing data that will have tests performed on. </returns>
+        public static List<Car> SeedCars(this AppDbContext appDbContext)
+        {
+            var cars = new List<Car>
+            {
+                new Car(2021,"Audi","X7",25,"22-SB-AZ"),
+                new Car(2021,"Audi","A6",30,"99-XX-89"),
+                new Car(2021,"BMW","A7",45,"AA-20-XX"),
+                new Car(2021,"Honda","A8",50,"15-BS-44")
+            };
+
+            foreach (var car in cars)
+            {
+                appDbContext.Cars.Add(car);
+            }
+
+            return cars;
+        }
+
+        //public static List<Booking> SeedBookings(this AppDbContext appDbContext)
+        //{
+        //    var bookings = new List<Booking> {
+        //        new Booking("xa1",1, new DateTime(2021,11,27),new DateTime(2021,11,30),50),
+        //        new Booking("2ac",2, new DateTime(2021,12,10),new DateTime(2021,12,15),20),
+        //        new Booking("4ac",3, new DateTime(2021,12,16),new DateTime(2021,12,25),25),
+        //        new Booking("66a",4, new DateTime(2021,12,20),new DateTime(2021,12,28),30)
+        //    };
+
+        //    foreach (var booking in bookings)
+        //    {
+        //        appDbContext.Bookings.Add(booking);
+        //    }
+
+        //    return bookings;
+        //}
+    }
+}
---
+++ Source/Car_Renting.Controllers.Tests/MockUserManager.cs
@@ -0,0 +1,62 @@
+﻿using Car_Renting.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_Renting.Controllers.Tests
+{
+    public class MockUserManager : UserManager<User>
+    {
+        private HashSet<string> _adminUserIds;
+
+        public User CurrentUser {get; set; }
+
+        public AppDbContext Context { get; private set; }
+
+        private MockUserManager()
+            : base(null, null, null, null, null, null, null, null, null)
+        {
+            throw new NotSupportedException();
+        }
+
+        public static MockUserManager Create(AppDbContext context)
+        {
+            // UserManager methods that are used are fully mocked out so we don't need a functional base class
+            var userManager = (MockUserManager)FormatterServices.GetUninitializedObject(typeof(MockUserManager));
+            userManager.Context = context;
+            return userManager;
+        }
+
+        public User AddUser(string email, string firstName, string lastName, string phoneNumber, bool isAdmin)
+        {
+            if (isAdmin)
+            {
+                 _adminUserIds.Add(email);
+            }
+
+            var user = new User {Id = email , UserName = email, Email = email, FirstName = firstName, LastName = lastName, PhoneNumber = phoneNumber };
+
+            Context.Users.Add(user);
+            Context.SaveChanges();
+
+            return user;
+        }
+
+        public override Task<User> GetUserAsync(ClaimsPrincipal principal)
+        {
+            return Task.FromResult(CurrentUser);
+        }
+
+        public override Task<bool> IsInRoleAsync(User user, string role)
+        {
+            return Task.FromResult(role == "Admin" && _adminUserIds.Contains(user.Email)));
+        }
+    }
+}
