cc605fea6bd8b5882f4566c9f336adc464ac5c4c
Giura Emanuel <emanuel1996a@gmail.com>
2021-11-30 10:34:43 +0200

    Finished implemeting SeedBookings method

--- Source/Car_Renting.Controllers.Tests/DbSetup.cs
+++ Source/Car_Renting.Controllers.Tests/DbSetup.cs
@@ -1,5 +1,4 @@
 ﻿using Car_Renting.Data;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -18,13 +17,13 @@ namespace Car_Renting.Controllers.Tests
             return dbContext;
         }
 
-        public static (MockUserManager UserManager, User AdminUser, User User) SeedUsers(this AppDbContext context)
+        public static MockUserManager SeedUsers(this AppDbContext context)
         {
             var userManager = MockUserManager.Create(context);
-            var admin = userManager.AddUser("giura.emanuel@gmail.com", "Giura", "Emanuel", "222555666", true); // admin
-            var user = userManager.AddUser("jason.bourne@gmail.com", "Jason", "Bourne", "555777999", false); // normal user;
+            userManager.AddUser("giura.emanuel@gmail.com", "Giura", "Emanuel", "222555666", true); // admin
+            userManager.AddUser("jason.bourne@gmail.com", "Jason", "Bourne", "555777999", false); // normal user;
 
-            return (userManager, admin, user);
+            return userManager;
         }
         /// <summary>
         /// Adds a list of 4 cars to the database.
@@ -48,22 +47,28 @@ namespace Car_Renting.Controllers.Tests
 
             return cars;
         }
+        
+        /// <summary>
+        /// <para>Adds bookings to both user and admin and returns them in a list in the following order:</para>
+        /// <para>Admin booking: StartDate: 2021-11-24 -- EndDate: 2021-11-29 -- Cost: 50</para>
+        /// <para>User past booking: StartDate: 2021-10-10 -- EndDate: 2021-10-29 -- Cost: 40</para>
+        /// <para>User current booking: StartDate: today -- EndDate: tomorrow -- Cost: 35</para>
+        /// <para>User future booking: StartDate: 15 days from today -- EndDate: 20 days from today -- Cost: 20</para>
+        /// <para></para>
+        /// </summary>
+        /// <param name="userManager"> Gets users.</param>
+        /// <param name="car"> Car details.</param>
+        public static List<Booking> SeedBookings(this MockUserManager userManager, Car car)
+        {
+            var bookings = new List<Booking>
+            {
+                new Booking(userManager.AdminUser.Id,car.Id, new DateTime(2021,11,24), new DateTime(2021,11,29),50),
+                new Booking(userManager.NormalUser.Id,car.Id, new DateTime(2021,10,10), new DateTime(2021,10,29),40),
+                new Booking(userManager.NormalUser.Id,car.Id, DateTime.Today, DateTime.Today.AddDays(1),35),
+                new Booking(userManager.NormalUser.Id,car.Id, DateTime.Today.AddDays(15), DateTime.Today.AddDays(20),20),
+            };
 
-        //public static List<Booking> SeedBookings(this AppDbContext appDbContext)
-        //{
-        //    var bookings = new List<Booking> {
-        //        new Booking("xa1",1, new DateTime(2021,11,27),new DateTime(2021,11,30),50),
-        //        new Booking("2ac",2, new DateTime(2021,12,10),new DateTime(2021,12,15),20),
-        //        new Booking("4ac",3, new DateTime(2021,12,16),new DateTime(2021,12,25),25),
-        //        new Booking("66a",4, new DateTime(2021,12,20),new DateTime(2021,12,28),30)
-        //    };
-
-        //    foreach (var booking in bookings)
-        //    {
-        //        appDbContext.Bookings.Add(booking);
-        //    }
-
-        //    return bookings;
-        //}
+            return bookings;
+        }
     }
 }
