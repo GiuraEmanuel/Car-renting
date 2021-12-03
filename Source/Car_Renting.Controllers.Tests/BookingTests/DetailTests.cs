a6f855dd1f27d3ee34649bf9cd65b98707ecedb8
Giura Emanuel <emanuel1996a@gmail.com>
2021-12-03 12:31:37 +0200

    Fix UserChecksHisOwnCancelledBookingDetails

--- Source/Car_Renting.Controllers.Tests/BookingTests/DetailTests.cs
+++ Source/Car_Renting.Controllers.Tests/BookingTests/DetailTests.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace Car_Renting.Controllers.Tests.BookingTests
 {
     [TestClass]
-    public class Detail
+    public class DetailTests
     {
         [TestMethod]
         public async Task UserChecksHisOwnBookingDetails()
@@ -108,20 +108,23 @@ namespace Car_Renting.Controllers.Tests.BookingTests
             var cars = DbSetup.SeedCars(context);
             var bookings = DbSetup.SeedBookings(context, user, cars[1]);
 
+            bookings[2].Cancel(20);
+            context.SaveChanges();
+
             var bookingController = new BookingController(context, userManager, null);
 
             // Act
-            var result = await bookingController.Detail(bookings[0].Id);
+            var result = await bookingController.Detail(bookings[2].Id);
 
             // Assert
             var viewResult = result.ShouldBeOfType<ViewResult>();
             viewResult.ViewName.ShouldBe(null);
 
             var model = viewResult.Model.ShouldBeOfType<BookingDetailsViewModel>();
-            model.BookingId.ShouldBe(bookings[0].Id);
-            model.TotalNumberOfDays.ShouldBe(19);
-            model.CancelRefundAmount.ShouldBe(null);
-            // past booking so refund should be 0
+            model.BookingId.ShouldBe(bookings[2].Id);
+            model.TotalNumberOfDays.ShouldBe(5);
+            model.CancelRefundAmount.ShouldBe(20);
+            // future booking already cancelled so refund should be 0
             model.RefundAmountIfCancelling.ShouldBe(0);
             model.Email.ShouldBe("jason.bourne@gmail.com");
             model.Model.ShouldBe(cars[1].Model);
