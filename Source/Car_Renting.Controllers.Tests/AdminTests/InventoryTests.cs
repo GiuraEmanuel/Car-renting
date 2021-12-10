8bddb526faa72ea4902386871c84a7b89bd1b757
Giura Emanuel <emanuel1996a@gmail.com>
2021-12-10 13:31:51 +0200

    Add InventoryTests

---
+++ Source/Car_Renting.Controllers.Tests/AdminTests/InventoryTests.cs
@@ -0,0 +1,41 @@
+﻿using Car_Renting.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Car_Renting.Data;
+
+namespace Car_Renting.Controllers.Tests.AdminTests
+{
+    [TestClass]
+    public class InventoryTests
+    {
+        [TestMethod]
+        public async Task ReturnAllActiveCarsFromInventory()
+        {
+            // Arrange
+            await using var context = DbSetup.Initialize();
+
+            var cars = DbSetup.SeedCars(context);
+
+            cars[0].Status = CarStatus.Deleted;
+
+            await context.SaveChangesAsync();
+
+            var adminController = new AdminController(context);
+
+            // Act
+            var result = await adminController.Inventory();
+
+            // Assert
+            var viewResult = result.ShouldBeOfType<ViewResult>();
+            viewResult.ViewName.ShouldBe(null);
+            viewResult.Model.ShouldBeOfType<InventoryViewModel>();
+            context.Cars.ShouldContain(car => car.Status == CarStatus.Active);
+        }
+    }
+}
