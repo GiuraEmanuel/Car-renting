d4a331653f3463cb04b0a13e4f9d306c663b21d5
Giura Emanuel <emanuel1996a@gmail.com>
2021-12-10 13:31:19 +0200

    Add DeleteVehicleTests

---
+++ Source/Car_Renting.Controllers.Tests/AdminTests/DeleteVehicleTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using Car_Renting.Data;
+using Car_Renting.Models;
+using Microsoft.AspNetCore.Mvc;
+using Shouldly;
+
+namespace Car_Renting.Controllers.Tests.AdminTests
+{
+    [TestClass]
+    public class DeleteVehiclePostTests
+    {
+        [TestMethod]
+        public async Task ReturnErrorMessageIfCarDoesNotExist()
+        {
+            // Arrange
+            using var context = DbSetup.Initialize();
+            var adminController = new AdminController(context);
+
+            // Act
+            var result = await adminController.DeleteVehicle(123);
+
+            // Assert
+            var viewResult = result.ShouldBeOfType<ViewResult>();
+            viewResult.ViewName.ShouldBe("ErrorMessage");
+            var model = viewResult.Model.ShouldBeOfType<ErrorMessageViewModel>();
+            model.Message.ShouldBe(ErrorMessages.CarDoesNotExist);
+        }
+
+        [TestMethod]
+        public async Task SuccessfullyDeleteCar()
+        {
+            // Arrange
+            using var context = DbSetup.Initialize();
+            var cars = DbSetup.SeedCars(context);
+
+            var adminController = new AdminController(context);
+
+            // Act
+            var result = await adminController.DeleteVehicle(cars[0].Id);
+
+            // Assert
+            var redirectResult = result.ShouldBeOfType<RedirectToActionResult>();
+            cars[0].Status.ShouldBe(CarStatus.Deleted);
+            redirectResult.ControllerName.ShouldBe(null);
+            redirectResult.ActionName.ShouldBe("Inventory");
+        }
+    }
+}
